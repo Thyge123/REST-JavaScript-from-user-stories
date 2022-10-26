302d2c022bb08872f939fc9bcd16ad2835beda33
Thyge123 <martintsh25@gmail.com>
2022-10-26 09:52:55 +0200

    Add project files.

---
+++ REST + JavaScript from user stories/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace REST___JavaScript_from_user_stories
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
